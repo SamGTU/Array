8b7c5f423e49d9e39e043742b7258fb2231f09f7
Shelkopryad <wera3pass001122@yandex.ru>
2015-10-17 18:37:31 +0400

    with inseard

--- MyArrayList/src/com/company/ArrayListImitation.java
+++ MyArrayList/src/com/company/ArrayListImitation.java
@@ -6,18 +6,28 @@ package com.company;
  */
 
 public class ArrayListImitation {
-    int[] start_array;
-    public ArrayListImitation(){
-       start_array = new int[0];
-    }
-    public void add(int elem) {
+    int[] start_array = new int [0];
+    void add(int elem) {
         int[] final_array = new int[start_array.length + 1];
-        for (int i = 0; i < final_array.length; i++) {
-            start_array[i] = final_array[i];
+        for (int i = 0; i < start_array.length; i++) {
+            final_array[i] = start_array[i];
         }
         final_array[final_array.length - 1] = elem;
         this.start_array = final_array;
     }
+public void inseard(int elem,int index){
+    int[] final_array = new int[start_array.length+1];
+    for (int i = 0 ; i < start_array.length; i ++){
+        if(i < index){
+            final_array[i] = start_array[i];
+        }else{
+            final_array[i] = start_array[i +1];
+        }
+    }
+    final_array[index] = elem;
+    this.start_array = final_array;
+}
+
 }
 
 
--- MyArrayList/src/com/company/Main.java
+++ MyArrayList/src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     ArrayListImitation ma = new ArrayListImitation();
-        //ma.add(1);
-        ma.add(4);
+        ma.add(1);
+        ma.inseard(2, 1);
         ma.add(9);
-        System.out.println(ma.toString());
+        System.out.println(ma.start_array[1]);
     }
 }
